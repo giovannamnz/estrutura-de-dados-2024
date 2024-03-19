c86d5d30544c941c41ed2706cdd2b931ec6d9efd
Giovanna Menezes <129130075+giovannamnz@users.noreply.github.com>
2024-03-19 09:22:26 -0300

    Delete hello-world.c

--- hello-world.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/*quebrando a maldição do hello wolrd :P */
-
-int main (int argc, char* argv[]){
-    printf("Hello World\n %s", argv[1]);
-    exit(0);
-}
