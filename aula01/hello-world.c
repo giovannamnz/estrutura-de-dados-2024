089ed0d585ede336a6badf290dc0fff425cd76be
Giovanna Menezes <129130075+giovannamnz@users.noreply.github.com>
2024-03-19 09:21:20 -0300

    Add files via upload

---
+++ aula01/hello-world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*quebrando a maldição do hello wolrd :P */
+
+int main (int argc, char* argv[]){
+    printf("Hello World\n %s", argv[1]);
+    exit(0);
+}
